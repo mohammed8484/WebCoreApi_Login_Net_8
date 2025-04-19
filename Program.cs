9956c76d0caf092bca54dc837119b84e48eb5cc0
Ali111q <93655964+Ali111q@users.noreply.github.com>
2025-04-19 11:34:20 +0300

    up date by mohanad

--- Program.cs
+++ Program.cs
@@ -3,18 +3,29 @@ using WebCoreApi_Login_Net8.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ✳️ إضافة خدمة CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<mydbcontext>(e => e.UseSqlServer(builder.Configuration.GetConnectionString("loginApi")));
+
 builder.Services.AddDbContext<mydbcontext>(options =>
     options.UseInMemoryDatabase("TestDb"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// ✳️ استخدام CORS (قبل UseAuthorization)
+app.UseCors("AllowAll");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
